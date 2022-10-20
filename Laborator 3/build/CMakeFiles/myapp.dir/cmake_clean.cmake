df8d19239a9b3acb1682379833412bf24ae595c8
Student <student@b418b-9.intranet.cs.upt.ro>
2022-10-20 17:05:23 +0300

    lab3

---
+++ Laborator 3/build/CMakeFiles/myapp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myapp.dir/lab.cpp.o"
+  "myapp"
+  "myapp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myapp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
